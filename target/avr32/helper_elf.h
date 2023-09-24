0c01a2760192b40c24299c92af2369ee89a487dd
Florian Göhler <mail@fgoehler.com>
2023-09-24 11:35:51 +0200

    Made filename declaration global for later use.

--- target/avr32/helper_elf.h
+++ target/avr32/helper_elf.h
@@ -22,7 +22,7 @@
 
 #include "elf.h"
 
-bool avr32_is_elf_file(char *filename);
+bool avr32_is_elf_file(const char *filename);
 void avr32_convert_elf_header(Elf32_Ehdr *header);
 uint32_t avr32_elf_convert_int(uint32_t num);
 uint16_t avr32_elf_convert_short(short num);
@@ -30,5 +30,6 @@ void avr32_elf_read_section_headers(Elf32_Ehdr *header, FILE* file, Elf32_Shdr**
 void avr32_elf_read_sh_string_table(Elf32_Ehdr *header, FILE* file, Elf32_Shdr** sh_table, char *sh_strtable);
 void avr32_elf_read_string_table(Elf32_Ehdr *header, FILE* file, Elf32_Shdr** sh_table, char *sh_strtable, char *strtable);
 void avr32_read_symtab(Elf32_Ehdr *header, FILE* file, Elf32_Shdr** sh_table, char *sh_strtable, char *strtab, Elf32_Sym** sym_tab);
+extern const char *AVR32_FIRMWARE_FILE;
 
 #endif //QEMU_AVR32_HELPER_ELF_H
